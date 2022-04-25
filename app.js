465bb0e7722e4247bcd2e0b1b9b4cf3a5791bde8
David <Rigby335@hotmail.com>
2022-04-25 16:14:42 -0700

    More button work

--- app.js
+++ app.js
@@ -5,17 +5,46 @@ const buttonThree = document.getElementById('button-three');
 const rightSpan = document.getElementById('right');
 const wrongSpan = document.getElementById('wrong');
 const totalSpan = document.getElementById('total');
+const shellOne = document.getElementById('shell-1');
+const shellTwo = document.getElementById('shell-2');
+const shellThree = document.getElementById('shell-3');
 
-let count = 0;
+let wins = 0;
+let losses = 0;
 
 buttonOne.addEventListener('click', () => {
-  // update the count
-  count++;
-  // update the view
-  rightSpan.textContent = count;
+    shellOne.classList.remove('reveal');
+    shellTwo.classList.remove('reveal');
+    shellThree.classList.remove('reveal');
+    const randomShell = Math.ceil(Math.random() * 3);
+    if (randomShell === 1) {
+        shellOne.classList.add('reveal');
+        right++;
+    } else if (randomShell === 2) {
+        shellTwo.classList.add('reveal');
+        wrong++;
+    } else {
+        shellThree.classList.add('reveal');
+        total++;
+    }
+    rightSpan.textContent = right;
+    wrongSpan.textContent = wrong;
+    totalSpan.textContent = total;
 });
 
+// let count = 0;
 
+// buttonOne.addEventListener('click', () => {
+//   // update the count
+//     count++;
+//   // update the view
+//     rightSpan.textContent = count;
+// });
+
+// buttonTwo.addEventListener('click', () => {
+//     count++;
+//     wrongSpan.textContent = count;
+// });
 
 // let state
 
